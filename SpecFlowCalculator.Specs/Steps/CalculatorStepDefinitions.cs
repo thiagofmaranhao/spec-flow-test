54e0712d35eeac58871f3ff3e7a6ebe4143ffd40
Thiago Maranhão <thiagofmaranhao@gmail.com>
2022-09-12 21:55:00 -0300

    Add solution and project

---
+++ SpecFlowCalculator.Specs/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+
+namespace SpecFlowCalculator.Specs.Steps;
+
+[Binding]
+public sealed class CalculatorStepDefinitions
+{
+    private readonly ScenarioContext _scenarioContext;
+    private readonly Calculator _calculator = new Calculator();
+    private int _result;
+
+    public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [Given("the first number is (.*)")]
+    public void GivenTheFirstNumberIs(int number)
+    {
+        _calculator.FirstNumber = number;
+    }
+
+    [Given("the second number is (.*)")]
+    public void GivenTheSecondNumberIs(int number)
+    {
+        _calculator.SecondNumber = number;
+    }
+
+    [When("the two numbers are added")]
+    public void WhenTheTwoNumbersAreAdded()
+    {
+        _result = _calculator.Add();
+    }
+
+    [Then("the result should be (.*)")]
+    public void ThenTheResultShouldBe(int result)
+    {
+        Assert.Equal(result, _result);
+    }
+
+    [When(@"the two numbers are subtracted")]
+    public void WhenTheTwoNumbersAreSubtracted()
+    {
+        _result = _calculator.Subtract();
+    }
+}
---
+++ SpecFlowCalculator/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace SpecFlowCalculator
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+
+        public int Subtract()
+        {
+            return FirstNumber - SecondNumber;
+        }
+    }
+}
